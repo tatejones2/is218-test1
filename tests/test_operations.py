98f50b5d5931ae6ebbba3cf8330da181278e3802
Tate Jones <tnj@njit.edu>
2025-09-30 15:47:12 -0400

    feature: implement multiply() with unit tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,10 @@
-from src.operations import add, subtract
+from src.operations import add, subtract, multiply
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_subtract():
     assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(4, 3) == 12
